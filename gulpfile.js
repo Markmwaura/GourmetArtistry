250b87e4ad93622a6b6ca875b6955c45558defb0
Mark Mwaura <markkmwaura@gmail.com>
2017-03-14 03:04:29 +0300

    enqueue style and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,57 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
 var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+    'bower_components/normalize.scss/sass',
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+    return gulp.src('scss/app.scss')
+        .pipe($.sass({
+                includePaths: sassPaths,
+                outputStyle: 'compressed' // if css compressed **file size**
+            })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('css'));
+});
+var JSFiles = [
+  './bower_components/foundation-sites/js/foundation.core.js',
+  './bower_components/what-input/src/what-input.js'
+];
+
+gulp.task('moveJSfiles', function (){
+  gulp.src(JSFiles,{}).pipe(gulp.dest('js/'));
+
+
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('browser-sync', function() {
+
+    var files = [
+        './style.css',
+        './*.php',
+        './template-parts/*.php',
+        './inc/*.php',
+        './js/*.js',
+        'css/app.css'
+    ];
+    //initilizes browser-sync
+    browserSync.init(files, {
+      proxy:"http://localhost:8080/Newtheme/",
+      notify:false
+
+    });
+
+
+});
+
+gulp.task('default', ['sass','browser-sync','moveJSfiles'], function() {
+    gulp.watch(['scss/**/*.scss'], ['sass']);
 });
